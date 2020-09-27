93ee7efcb55f49ec4e8af9148ff47ec242970995
ZHENK <chengzhenyang@gmail.com>
2020-09-28 07:10:53 +1300

    first commit

---
+++ android/app/src/main/java/com/syloconnectedapptemplate/MainApplication.java
@@ -0,0 +1,93 @@
+package com.syloconnectedapptemplate;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.airbnb.android.react.lottie.LottiePackage;
+import com.oney.WebRTCModule.WebRTCModulePackage;
+import org.libsodium.rn.RCTSodiumPackage;
+import com.oblador.keychain.KeychainPackage;
+import com.bitgo.randombytes.RandomBytesPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.horcrux.svg.SvgPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new AsyncStoragePackage(),
+          new LottiePackage(),
+          new WebRTCModulePackage(),
+          new RCTSodiumPackage(),
+          new KeychainPackage(),
+          new RandomBytesPackage(),
+          new ReanimatedPackage(),
+          new SvgPackage(),
+          new RNCameraPackage(),
+          new VectorIconsPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ ios/demo.swift
@@ -0,0 +1,9 @@
+//
+//  demo.swift
+//  CryptoTracker
+//
+//  Created by Darpan on 29/10/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
